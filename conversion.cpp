2b5130ef57680ff1f6f9f09b2ea420e2b6e93b1f
Amir <amir.raoofy@tum.de>
2016-02-03 16:56:37 +0100

    MOD: MPI read/write was fixed

--- conversion.cpp
+++ conversion.cpp
@@ -8,7 +8,7 @@ int main (int argc, char **argv){
 
 	double * data;
 	FILE *fp;
-	int matrices_a_b_dimensions[2];
+	int buf[2];
 
 	MPI_File fh;
 	MPI_Init(&argc, &argv);
@@ -16,19 +16,26 @@ int main (int argc, char **argv){
 	MPI_File_open(MPI_COMM_WORLD, argv[2], MPI_MODE_RDWR, MPI_INFO_NULL, &fh);
 
 	if ((fp = fopen (argv[1], "r")) != NULL){
-		fscanf(fp, "%d %d\n", matrices_a_b_dimensions, matrices_a_b_dimensions+1);
-		data = new double [matrices_a_b_dimensions[0] * matrices_a_b_dimensions[1]];
-		for (int i=0; i < matrices_a_b_dimensions[0] * matrices_a_b_dimensions[1]; i++)
-			fscanf(fp, "%lf", data+i);
+		fscanf(fp, "%d %d", buf, buf+1);
+		data = new double [(int)(buf[0] * buf[1])];
+		for (int i=0; i < (int)(buf[0] * buf[1]); i++)
+			fscanf(fp, "%lf ", data+i);
 	} else {
 		fprintf(stderr, "error opening file for matrix A (%s)\n", argv[1]);
 		exit (EXIT_FAILURE);
 	}
 
-	MPI_File_write(fh, matrices_a_b_dimensions, 2, MPI_INT, MPI_STATUS_IGNORE);
-	MPI_File_write(fh, data, matrices_a_b_dimensions[0] * matrices_a_b_dimensions[1], MPI_DOUBLE, MPI_STATUS_IGNORE);
-	MPI_File_close(&fh);
 
+	MPI_File_write(fh, buf,  2,                      MPI_INT,    MPI_STATUS_IGNORE);
+	MPI_File_write(fh, data, (int)(buf[0] * buf[1]), MPI_DOUBLE, MPI_STATUS_IGNORE);
+	
+
+	MPI_File_read(fh, buf, 2, MPI_INT, MPI_STATUS_IGNORE);
+//	printf ("%d\n%d\n%lf\n%lf\n" ,buf[0], buf[1],buf[2],buf[3]);
+	printf ("%d\n%d\n%lf\n%lf\n" ,buf[0], buf[1],data[0], data[1]);
+
+
+	MPI_File_close(&fh);
 	MPI_Finalize();
 
 return 0;
